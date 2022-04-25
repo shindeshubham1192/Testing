f89952f9ea4a5d5de760dbdcffc35ecc7f45ce19
shubham <shubham@ADMINRG-5N1RFN0>
2022-04-25 19:56:55 +0530

    by shubham25

--- Github1/src/com/github/Demo.java
+++ Github1/src/com/github/Demo.java
@@ -1,8 +1,16 @@
 package com.github;
 
 public class Demo {
+	
+	public void m1()
+	{
+		System.out.println("M1 method is called");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("hello this is first github project");
+		
+		Demo d = new Demo();
+		d.m1();
 	}
 }
